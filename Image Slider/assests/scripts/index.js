4142651ace990f10ee4074370e3698e4ff0f33a5
juicyR <matovureginald@gmail.com>
2020-10-27 16:50:08 +0300

    Sigh

--- Image Slider/assests/scripts/index.js
+++ Image Slider/assests/scripts/index.js
@@ -1,41 +1,24 @@
 //Necessary vars to start with
 let image = document.querySelectorAll('img');
 let time = 3000;
-let i;
 let image1;
 //Displaying all images block
-for (i = 0; i < image.length; i++) {
-    let myTimer = setInterval(() => {
-                    displayBlock()
-                  }, time);
-    function displayBlock() {
+let timing1 = setInterval(() => {
+    for (let i = 0; i < image.length; i++) {
         image[i].style.display = 'block';
     }
-    displayBlock()
-    myTimer
-    //Looping through the images being displayed.
-         for (image1 = 0; image1 < image.length; image1++) {
-            function imageChange() {
-                image[i -= image1].style.display = 'none';
-            }
-            let myTimer2 = setInterval(() => {
-                                imageChange()
-                           }, time);
-            imageChange()
-            myTimer2
-            if (i === 7) {
-                i === 0
-            } else {
-                i++
-            }
-         }
-}
-//Image freeze on hover
-// image.addEventListener("click", stopFunction);
-
-// function stopFunction() {
-//     clearInterval(myTimer);
+}, 3000);
+timing1
+// for (let i = 0; i < image.length; i++) {
+//     let timing1 = setInterval(() => {
+//         image[0].style.display = 'none';
+//     }, 3000);
+//     timing1
 // }
+//Looping through the images being displayed.
+
+//Image freeze on hover
+
 //Navigation bar for images
 
 //Images nav on click
